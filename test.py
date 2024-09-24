7150a898427348265948c8d0e47435d6b23cd46d
Ajeet Athwal <ajeet_athwal@hotmail.co.uk>
2024-09-24 21:34:33 +0100

    add failing unit test

--- test.py
+++ test.py
@@ -5,3 +5,9 @@ def test_return_hello():
     return_str = return_hello(str)
 
     assert return_str == "hello " + str
+
+def test_return_hello2():
+    str = "world"
+    return_str = return_hello(str)
+
+    assert return_str == "hello" + str    
