bbafaf0c7edfe6e081504b0532d3320c5afedf56
Ajeet Athwal <ajeet_athwal@hotmail.co.uk>
2024-09-24 21:18:11 +0100

    add functions for printing and returning hello

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+def print_hello(str: str) -> None:
+    print(f"hello {str}")
+
+def return_hello(str: str) -> str:
+    return "hello " + str
+
+
+if __name__ == "__main__":
+    print_hello("world")
+    print(return_hello("world"))
